10f52a22c4f8c124e218c4feed0cd29b2d10ff20
Rostislav Svoboda <Rostislav.Svoboda@gmail.com>
2022-10-10 13:07:17 +0200

    Fix byte-compilation warnings, change prefix s/my=/kb-/

--- kill-buffers.el
+++ kill-buffers.el
@@ -1,6 +1,6 @@
-;;; kill-buffers.el --- Jump to the Last Edit Location, regardless of the file.
+;;; kill-buffers.el --- Jump to the Last Edit Location, regardless of the file.  -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2020 - 2023 Rostislav Svoboda
+;; Copyright (C) 2020 - 2024 Rostislav Svoboda
 
 ;; Authors: Rostislav Svoboda <Rostislav.Svoboda@gmail.com>
 ;; Version: N/A
@@ -19,15 +19,31 @@
 ;; and then
 ;;   (kill-buffers :location "<path/to/the/cloned-repo>")
 
-;; (require 'goto-chg)
+;; TODO see Prot's
+;; https://protesilaos.com/codelog/2020-08-03-emacs-custom-functions-galore/
+;; (defun prot/kill-buffer-current (&optional arg)
+;;   "Kill current buffer or abort recursion when in minibuffer."
+;;   (interactive "P")
+;;   (if (minibufferp)
+;;       (abort-recursive-edit)
+;;     (kill-buffer (current-buffer)))
+;;   (when (and arg
+;;              (not (one-window-p)))
+;;     (delete-window)))
+;; :bind (("s-m" . prot/window-single-toggle)
+;;        ("s-k" . prot/kill-buffer-current))
 
-(defun my=kill-buffers--forcefully (regexp &optional internal-too)
+(require 'term)             ;; for term-send-eof
+(require 'cl)               ;; for cl-remove-if-not, cl-find
+(require 'cider-connection) ;; for cider-quit
+
+(defun kb-kill-buffers--forcefully (regexp &optional internal-too)
   "Kill - WITHOUT ASKING - buffers whose name matches the specified REGEXP.
 See the `kill-matching-buffers` for grateful killing. The optional 2nd argument
 indicates whether to kill internal buffers too.
 
 Returns the count of killed buffers."
-  (let* ((buffers (remove-if-not
+  (let* ((buffers (cl-remove-if-not
                    (lambda (buffer)
                      (let ((name (buffer-name buffer)))
                        (and name (not (string-equal name ""))
@@ -39,7 +55,7 @@ Returns the count of killed buffers."
 
 ;; TODO see also packages easy-kill, easy-kill-extras, kill-or-bury-alive, kmb,
 ;; omni-kill, viking-mode
-(defun my=kill-buffers--force (regexp &optional internal-too)
+(defun kb-kill-buffers--force (regexp &optional internal-too)
   "Kill - WITHOUT ASKING - buffers whose name matches the specified REGEXP.
 See the `kill-matching-buffers` for grateful killing. The optional 2nd argument
 indicates whether to kill internal buffers too.
@@ -47,44 +63,46 @@ indicates whether to kill internal buffers too.
 Returns a message with the count of killed buffers."
   (interactive "sKill buffers matching this regular expression: \nP")
   (message "%d buffer(s) killed."
-           (my=kill-buffers--forcefully regexp internal-too)))
+           (kb-kill-buffers--forcefully regexp internal-too)))
 
-(defcustom my=magit-unwanted-modes
+(defcustom kb-magit-unwanted-modes
   '(magit-status-mode
     magit-log-mode
     magit-diff-mode
     magit-revision-mode
     magit-stash-mode
     magit-process-mode)
-  "A buffer with matching major-mode is killed by my=kill-buffers--magit."
+  "A buffer with matching major-mode is killed by kb-kill-buffers--magit."
   ;; :package-version '(kill-buffers . "1.0.0")
+  :group 'convenience
   :type 'list)
 
-(defun my=kill-buffers--magit ()
+(defun kb-kill-buffers--magit ()
   "Kill all Magit buffers."
   (interactive)
-  ;; (my=kill-buffers--forcefully "\*magit: .*\\|\*magit-.*")
+  ;; (kb-kill-buffers--forcefully "\*magit: .*\\|\*magit-.*")
   (save-excursion
     (let ((count 0))
       (dolist (buffer (buffer-list))
         (set-buffer buffer)
-        (when (find major-mode my=magit-unwanted-modes)
+        (when (cl-find major-mode kb-magit-unwanted-modes)
           (setq count (1+ count))
           (kill-buffer buffer)))
       (message "Killed %i Magit buffer(s)." count))))
 
-(defcustom my=all-unwanted--modes
-  (append my=magit-unwanted-modes
+(defcustom kb-all-unwanted--modes
+  (append kb-magit-unwanted-modes
           '(
             dired-mode
             Man-mode
             woman-mode
             ))
-  "A buffer with matching major-mode is killed by my=kill-buffers--unwanted."
+  "A buffer with matching major-mode is killed by kb-kill-buffers--unwanted."
   ;; :package-version '(kill-buffers . "1.0.0")
+  :group 'convenience
   :type 'list)
 
-(defcustom my=unwanted-buffers
+(defcustom kb-unwanted-buffers
   ;; prefer explicit listing of unwanted buffers
   '(
     "*Async-native-compile-log*"
@@ -122,11 +140,12 @@ Returns a message with the count of killed buffers."
     ;; "*spacemacs*" ; needed for ~SPC f e U~ M-x configuration-layer/update-packages
     "*vc*"
     )
-  "A buffer with a name from this list is killed by my=kill-buffers--unwanted."
+  "A buffer with a name from this list is killed by kb-kill-buffers--unwanted."
   ;; :package-version '(kill-buffers . "1.0.0")
+  :group 'convenience
   :type 'list)
 
-(defun my=kill-buffers--unwanted ()
+(defun kb-kill-buffers--unwanted ()
   "Kill all unwanted buffers and delete other windows so that only one remains
 displayed."
   (interactive)
@@ -135,17 +154,17 @@ displayed."
       (dolist (buffer (buffer-list))
         (set-buffer buffer)
         ;; find out buffer's major mode: (message "%s" major-mode)
-        (when (find major-mode my=all-unwanted--modes)
+        (when (cl-find major-mode kb-all-unwanted--modes)
           (kill-buffer buffer)
           (setq count (1+ count))))
-      (dolist (buffer my=unwanted-buffers)
+      (dolist (buffer kb-unwanted-buffers)
         (when (get-buffer buffer) ; check if buffer exists
           (kill-buffer buffer)
           (setq count (1+ count))))
       ;; (delete-other-windows)
       (message "Buffer(s) killed: %i" count))))
 
-(defun my=kill-buffers--dired ()
+(defun kb-kill-buffers--dired ()
   "Kill all dired buffers."
   (interactive)
   (save-excursion
@@ -157,7 +176,7 @@ displayed."
           (kill-buffer buffer)))
       (message "Killed %i dired buffer(s)." count))))
 
-(defun my=close-buffer ()
+(defun kb-close-buffer ()
   "Clojure repl buffer needs to invoke its own kill function"
   (interactive)
   (cond
@@ -175,7 +194,7 @@ displayed."
 
   ;; There's no need to place the call of (balance-windows-area) outside of this
   ;; package to minimizing package dependencies, via e.g.
-  ;;   (advice-add #'my=close-buffer :after balance-windows-area).
+  ;;   (advice-add #'kb-close-buffer :after balance-windows-area).
   ;; `balance-windows-area' is from windows.el, which is part of GNU Emacs.
   (balance-windows-area))
 
